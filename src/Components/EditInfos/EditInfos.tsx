c338f5d07ff29f67bdc5419be2b8697d2157e766
Luís Felipe Torres Galindo <130312172+lftorresg@users.noreply.github.com>
2023-08-28 00:34:35 -0300

    :iphone: responsiveness on edit page fixed

--- src/Components/EditInfos/EditInfos.tsx
+++ src/Components/EditInfos/EditInfos.tsx
@@ -1,78 +1,67 @@
 import classes from "./EditInfos.module.css";
 
 const EditInfos = () => {
-  document.addEventListener("DOMContentLoaded", function () {
-    const inputDate = document.querySelector(".dateInput") as HTMLInputElement;
-
-    inputDate.addEventListener("input", function () {
-      const inputValue = inputDate.value.replace(/\D/g, "");
-      const formattedValue = inputValue
-        .replace(/^(\d{2})(\d)/, "$1/$2")
-        .replace(/^(\d{2}\/\d{2})(\d{0,4})/, "$1/$2");
-
-      inputDate.value = formattedValue;
-    });
-  });
-
   return (
     <section className={classes.container}>
       <section className={classes.editText}>
         <h1>Editar informações</h1>
       </section>
-      <section className={classes.column}>
-        <section className={classes.field}>
-          <input
-            type="text"
-            placeholder="Profissão"
-            className={classes.input}
-          />
+      <div className={classes.wrap}>
+        <section className={classes.column}>
+          <section className={classes.field}>
+            <input
+              type="text"
+              placeholder="Profissão"
+              className={classes.input}
+            />
+          </section>
+          <section className={classes.field}>
+            <input type="text" placeholder="Nome" className={classes.input} />
+          </section>
+          <section className={classes.field}>
+            <input type="text" placeholder="Cidade" className={classes.input} />
+          </section>
+          <section className={classes.field}>
+            <input type="text" placeholder="País" className={classes.input} />
+          </section>
+          <section className={classes.field}>
+            <input
+              type="date"
+              placeholder="DD/MM/AAAA"
+              className={classes.input}
+            />
+          </section>
+          <section className={classes.field}>
+            <input
+              type="password"
+              placeholder="Senha"
+              className={classes.input}
+            />
+          </section>
+          <section className={classes.field}>
+            <input
+              type="password"
+              placeholder="Repetir senha"
+              className={classes.input}
+            />
+          </section>
         </section>
-        <section className={classes.field}>
-          <input type="text" placeholder="Nome" className={classes.input} />
+        <section className={classes.column}>
+          <section className={classes.selectField}>
+            <select className={classes.select}>
+              <option disabled selected>
+                Relacionamento
+              </option>
+              <option>Solteiro(a)</option>
+              <option>Em um relacionamento sério</option>
+              <option>Noivo(a)</option>
+              <option>Casado(a)</option>
+            </select>
+          </section>
         </section>
-        <section className={classes.field}>
-          <input type="text" placeholder="Cidade" className={classes.input} />
-        </section>
-        <section className={classes.field}>
-          <input type="text" placeholder="País" className={classes.input} />
-        </section>
-        <section className={classes.field}>
-          <input
-            type="date"
-            placeholder="DD/MM/AAAA"
-            className={classes.input}
-          />
-        </section>
-        <section className={classes.field}>
-          <input
-            type="password"
-            placeholder="Senha"
-            className={classes.input}
-          />
-        </section>
-        <section className={classes.field}>
-          <input
-            type="password"
-            placeholder="Repetir senha"
-            className={classes.input}
-          />
-        </section>
-      </section>
-      <section className={classes.column}>
-        <section className={classes.selectField}>
-          <select className={classes.select}>
-            <option disabled selected>
-              Relacionamento
-            </option>
-            <option>Solteiro(a)</option>
-            <option>Em um relacionamento sério</option>
-            <option>Noivo(a)</option>
-            <option>Casado(a)</option>
-          </select>
-        </section>
-      </section>
+      </div>
       <section className={classes.field}>
-        <a href="/">Salvar</a>
+        <a href="/profile">Salvar</a>
       </section>
     </section>
   );
